23dc5018869dcc48dd9007cf393744c99b883216
Lukas Jackowski <l.jackowski@agorate.de>
2022-06-18 20:57:00 +0200

    Make beat into an event instead of a resource

--- src/beat.rs
+++ src/beat.rs
@@ -1,19 +1,16 @@
-//! Resource that keeps track of beat detection signals
+//! Components and systems related to beat signals
 
 use bevy::prelude::*;
 
-/// A resource that keeps track of beat signals
+/// Resource that counts how many beats have been received
 #[derive(Component)]
-pub struct Beat {
-    // True if a beat was received at beginning of frame
-    beat: bool,
-    // A counter that is increased on every beat
-    beat_counter: u64,
+pub struct BeatCounter {
+    // Counter is increased on every beat
+    count: u64,
 }
 
-impl Beat {
-    /// Returns true if a beat occured at beginning of frame
-    pub fn get_beat(&self) -> bool { self.beat }
-    /// Returns the beat counter
-    pub fn get_beat_counter(&self) -> u64 { self.beat_counter }
+/// Event that is emitted when a beat occurs
+pub struct BeatEvent{
+    // Value from BeatCounter
+    count: u64
 }
